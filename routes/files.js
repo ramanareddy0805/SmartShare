100955cd90d8618b2b899db4db60d5c907b58aa0
Ramanareddy Venkata <ramanareddylpu@gmail.com>
2022-11-11 19:25:42 +0530

    send_email

--- routes/files.js
+++ routes/files.js
@@ -46,7 +46,41 @@ router.post('/', (req, res) => {
 
     });
 
-    
+    router.post('/send', async(req, res) => {
+        const {uuid, emailTo, emailFrom} = req.body;
+        //validate the request
+
+        if(!uuid || !emailFrom || !emailTo){
+            return res.status(422).send({error: 'All fields are required.'});
+        }
+
+        //get data from database
+        const file = await File.findOne({ uuid: uuid});
+        if(file.sender){
+            return res.status(422).send({error: 'Email already sent.'});
+
+        }
+
+        file.sender = emailFrom;
+        file.receiver = emailTo;
+        const response = await file.save();
+
+        //send email
+        const sendMail = require('../services/emailService');
+        sendMail({
+            from: emailFrom,
+            to: emailTo,
+            subject: "SmartShare File Sharing",
+            text: `${emailFrom} has shared a file with you`,
+            html: require('../services/emailTemplate')({ 
+                emailFrom: emailFrom,
+                downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+                size: parseInt(file.size/1000) + 'KB',
+                expires: '24 Hours'
+            })
+        });
+        
+    });
 
     //send the link as response
 
